7d5ab618c5c016e4e7d524b8fbfab8498f8fe8d5
vampirictutor <vampirictutor@gmail.com>
2018-09-12 15:29:50 +0800

    working on BalanceSheet

--- db_operator.py
+++ db_operator.py
@@ -8,34 +8,98 @@ import subprocess
 
 import data_struct 
 
+
  # 打开DB，并酌情建表，返回 sqlite3.Connection
 def get_db_conn():
     conn = sqlite3.connect( data_struct.DB_PATH)
     conn.text_factory = str
  
-    sql = ''' CREATE TABLE IF NOT EXISTS SecurityInfo (
-       code   TEXT 
-       , name TEXT
-       , dir  TEXT
-       , PRIMARY KEY( code)
-       )
-    '''
-    conn.execute( sql) 
+    sql = ''' CREATE TABLE IF NOT EXISTS BalanceSheetDay (
+       id  INTERGER
+       , code   TEXT 
+       , day    TEXT
+       , pubDate TEXT
+       , statDate TEXT
+       , cash_equivalents  NUMERIC
+       , settlement_provi  NUMERIC
+       , lend_capital      NUMERIC
+       , trading_assets    NUMERIC
+       , bill_receivable   NUMERIC
+       , account_receivable NUMERIC
+       , advance_payment    NUMBER 
+       , insurance_receivables     NUMERIC
+       , reinsurance_receivables    NUMERIC
+       , reinsurance_contract_reserves_receivable    NUMERIC
+       , interest_receivable    NUMERIC
+       , dividend_receivable     NUMERIC
+       , other_receivable        NUMERIC
+       , bought_sellback_assets      NUMERIC
+       , inventories               NUMERIC
+       , non_current_asset_in_one_year     NUMERIC
+       , other_current_assets         NUMERIC
+       , total_current_assets         NUMERIC
+       , loan_and_advance             NUMERIC
+       , hold_for_sale_assets         NUMERIC
+       , hold_to_maturity_investments        NUMERIC
+       , longterm_receivable_account         NUMERIC
+       , longterm_equity_invest              NUMERIC
+       , investment_property                 NUMERIC
+       , fixed_assets                        NUMERIC
+       , constru_in_process                  NUMERIC
+       , construction_materials              NUMERIC
+       , fixed_assets_liquidation            NUMERIC
+       , biological_assets                   NUMERIC
+       , oil_gas_assets                      NUMERIC
+       , intangible_assets                   NUMERIC
+       , development_expenditure             NUMERIC
+       , good_will                           NUMERIC
+       , long_deferred_expense               NUMERIC
+       , other_non_current_assets            NUMERIC
+       , total_non_current_assets            NUMERIC
+       , total_assets                        NUMERIC
+       , shortterm_loan                      NUMERIC
+       , borrowing_from_centralbank          NUMERIC
+       , borrowing_capital                   NUMERIC
+       , trading_liability    NUMERIC
+       , notes_payable        NUMERIC 
+       , accounts_payable     NUMERIC 
+       , advance_peceipts     NUMERIC 
+       , sold_buyback_secu_proceeds    NUMERIC 
+       , commission_payable       NUMERIC 
+       , salaries_payable         NUMERIC 
+       , taxs_payable             NUMERIC 
+       , interest_payable    NUMERIC 
+       , dividend_payable    NUMERIC 
+       , other_payable       NUMERIC 
+       , reinsurance_payables NUMERIC 
+       , insurance_contract_reserves  NUMERIC
+       , proxy_secu_proceeds    NUMERIC
+       , receivings_from_vicariously_sold_securities NUMERIC
+       , non_current_liability_in_one_year NUMERIC
+       , other_current_liability NUMERIC 
+       , total_current_liability NUMERIC 
+       , longterm_loan    NUMERIC 
+       , bonds_payable    NUMERIC 
+       , longterm_account_payable   NUMERIC 
+       , specific_account_payable   NUMERIC 
+       , estimate_liability         NUMERIC 
+       , deferred_tax_liability     NUMERIC 
+       , other_non_current_liability NUMERIC 
+       , total_non_current_liability NUMERIC
+       , paidin_capital     NUMERIC
+       , capital_reserve_fund NUMERIC 
+       , treasury_stock       NUMERIC 
+       , specific_reserves    NUMERIC 
+       , surplus_reserve_fund NUMERIC 
+       , ordinary_risk_reserve_fund  NUMERIC 
+       , retained_profit      NUMERIC 
+       , foreign_currency_report_conv_diff NUMERIC 
+       , equities_parent_company_owners    NUMERIC
+       , minority_interests    NUMERIC
+       , total_owner_equities  NUMERIC
+       , total_sheet_owner_equities NUMERIC
 
-    sql = ''' CREATE TABLE IF NOT EXISTS MdHis (
-       code   TEXT 
-       , t_day      TEXT
-       , t_week_day TEXT
-       , open  NUMERIC
-       , close NUMERIC
-       , high  NUMERIC
-       , low   NUMERIC
-       , delta_r NUMERIC
-       , volume  NUMERIC
-       , amount  NUMERIC
-       , delta_alpha NUMBER 
-       , turnover_r NUMERIC
-       , PRIMARY KEY( code,t_day)
+       , PRIMARY KEY( id)
        )
     '''
     conn.execute( sql) 
