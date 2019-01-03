7034f817423966d3be81da5cbfbffed40a406341
vampirictutor <vampirictutor@gmail.com>
2019-01-03 23:01:23 +0800

    yes , 'buy_worst'

--- magic.py
+++ magic.py
@@ -13,6 +13,7 @@ import collections
 from   sqlalchemy.sql  import select as alch_select
 from   sqlalchemy.sql  import text   as alch_text
 import  pandas as pd
+import  math
 
 # 其他第三方包
 import  jqdatasdk as jq
@@ -1390,12 +1391,41 @@ def sim_rotate( his_data,  max_hold, base_code, start_day = "", end_day = ""):
 
     return (result ,  trans_num , trans_cost )
 
+# 如果code不在 md_that_day中，则调用jq api获取md
+def smart_get_md_close( t_day, code, md_that_day ):
+    if code in md_that_day:
+        # ‘行情’ 是  [收盘价，前日收盘，涨幅， 涨停标志，停牌标志]
+        return md_that_day[code ][0] 
+ 
+    df = jq.get_price(code
+            , start_date= t_day, end_date=t_day
+            , frequency='daily'
+               #  默认是None(表示[‘open’, ‘close’, ‘high’, ‘low’, ‘volume’, ‘money’]这几个标准字段)
+            , fields=['open', 'close', 'high', 'low', 'volume', 'money', 'high_limit', 'low_limit', 'pre_close', 'paused']
+            , skip_paused=False
+            , fq='pre'
+            )
+ 
+    row_count = len(df.index)
 
-# 买最糟卖最好轮换策略：
+    if row_count < 1:
+        raise Exception ("无法获得%s于%s的行情" %(code, t_day) )
+
+    p = df['close'].iloc[0]
+
+    #if math.isnan(p):
+    #    print df 
+    #    raise  Exception ("无法获得%s于%s的收盘是NaN" %(code, t_day) )
+
+    return p
+
+
+# 买最糟轮换策略：
 #     根据指标(目前是N日涨幅)从高到底排名。
-#     前M名如果>0，则各给1/M的仓位。
-#     卖出标准是指标名次低于sell_threshold 或者 指标<0.
-#     有空仓则依据建仓标准补进仓位。
+#     每20天做如下操作:
+#     最指标最差的M名，做为买入对象。
+#     不是‘买入对象’的持仓都卖出。
+#     然后买入不是持仓的‘买入对象’。
 # Input:  2-D array 'md_his'
 #         日期   各脚行情  各脚指标
 #         ...
@@ -1404,8 +1434,7 @@ def sim_rotate( his_data,  max_hold, base_code, start_day = "", end_day = ""):
 #         日期  基准收盘价   策略净值 交易次数  换仓详细  
 #         ...
 #
-def sim_rotate_buy_worst( his_data,  max_hold, base_code, start_day = "", end_day = ""):    
-    sell_threshold = max_hold + 2
+def sim_rotate_buy_worst( his_data,  max_hold, howlong,  base_code, start_day = "", end_day = ""):    
 
     if len(his_data) == 0:
         raise Exception("没有行情历史数据。"  );
@@ -1421,46 +1450,56 @@ def sim_rotate_buy_worst( his_data,  max_hold, base_code, start_day = "", end_da
 
     we_hold =  data_struct.make_init_shares(INITIAL_BALANCE, max_hold)  # 我们的持仓
 
+    real_tday_num  = 0
+
     for i, row in enumerate(his_data):
-
-        t_day = row[0]
-        #print "T_Day %s,  we hold %s" % (row[0], we_hold)
-
-        md_that_day      = row[1]   #当日行情    
-        indices_that_day = row[2]   #当日指标   
-
-        if i == 0 :
-            # 第一天，没有操作 ，也没有损益
-            r_that_day = []
-            r_that_day.append( t_day )
-            
-            md_of_base = md_that_day[base_code]
-            
-            r_that_day = [t_day, md_of_base[0],  INITIAL_BALANCE, None      ,None ]
-            #                    基准收盘价      策略净值         换仓提示   换仓明细
  
-            result.append( r_that_day )
-
-            continue 
-        
+        t_day = row[0]
         if "" != start_day and t_day < start_day:
             # 略过
             continue
 
         if "" != end_day and t_day >= end_day:
             break
-     
+ 
+        # print "\n============\nT_Day %s,  we hold\n %s" % (row[0], we_hold)
+
+        md_that_day      = row[1]   #当日行情    
+        indices_that_day = row[2]   #当日指标   
+
         
-        # 昨日本策略的收盘价
-        if len(result) > 0:
-            y_policy  = result[ len(result) - 1 ][2]    
-        else:
-            y_policy = INITIAL_BALANCE 
+        if 1 != (real_tday_num % howlong):
+            # 第一天按照第0天的收盘价和指标模拟操作
+            # 每howlong天操作一次
+
+            # 不操作，就更新一下持仓价格
+            for one_pos in we_hold.pos_entries:
+                if one_pos.is_blank():
+                    continue
+                p = smart_get_md_close(t_day,one_pos.code, md_that_day)
+
+                if math.isnan(p):
+                    print "WARN: %s , %s 的收盘是NaN，不更新持仓价格" % (t_day, one_pos.code)
+                else:
+                    one_pos.now_price = p
+            
+            md_of_base = md_that_day[base_code]
+            t_policy = we_hold.get_value()  
+            r_that_day = [t_day, md_of_base[0],  t_policy,   None      ,None ]
+            #                    基准收盘价      策略净值    换仓提示   换仓明细
+ 
+            result.append( r_that_day )
+
+            real_tday_num = real_tday_num + 1
+            continue 
+ 
+        # 要操作了
 
         # 这里有一个近似的假设：
         # 我们可以基于昨日的指标，按照昨日的收盘价，进行操作(记作今日操作)，并把操作的损益反映于今日。
         
         #昨日行情
+        y_date    =  his_data[i - 1][0]
         y_md      =  his_data[i - 1][1]
         #print 
         #util.bp(y_md)
@@ -1468,12 +1507,15 @@ def sim_rotate_buy_worst( his_data,  max_hold, base_code, start_day = "", end_da
         #昨日指标  {code1:指标数组1, code2:指标数组2, ... }
         y_indices =  his_data[i - 1][2]
 
-# 简单的轮换策略：
-#     根据昨日涨幅从从高到底排名。
-#     前M名如果>0，则各给1/M的仓位。
-        # 指标数组:  [可买标志，三日累计涨幅] 
+        # 指标数组:  [可买标志，howlong日累计涨幅] 
         WHICH_INDI = 1 # 我们取指标数组里哪一个指标?
+
+        # 按指标从小到大排序，故‘buy_worst’
+        #sorted_y_indices = sorted ( y_indices.items(), key=lambda sec:sec[1][WHICH_INDI])
+        
+        # 尝试buy_best，于是就成了 'buy_best'
         sorted_y_indices = sorted ( y_indices.items(), key=lambda sec:sec[1][WHICH_INDI], reverse=True)
+        
         #print 
         #util.bp(sorted_y_indices)
 
@@ -1481,6 +1523,31 @@ def sim_rotate_buy_worst( his_data,  max_hold, base_code, start_day = "", end_da
         to_hold = []   # 继续持仓的份额编号
         to_sell = []   # 要卖出的份额编号
         to_buy  = []   # 要买进的代码
+ 
+        # 撸一遍昨日M“强”，看看有哪些要买进
+        max_buy = max_hold
+        rank = 1
+        for code,indi  in sorted_y_indices:
+            if max_buy <=0 :
+                break
+            
+            # 比较基准不买
+            if code == FH_BASE_CODE:
+                continue
+            
+            if  not indi[0]:
+            #   可买标志  
+                continue
+            
+            if code not in md_that_day:
+                # 当日该code已经不在指数成份里
+                continue
+
+            #print "应当买入 %s，排名%d" % (code, rank)
+            to_buy.append(code)
+            max_buy = max_buy - 1
+            rank = rank + 1
+
 
         #print "%s:" % t_day 
         #print we_hold 
@@ -1494,51 +1561,18 @@ def sim_rotate_buy_worst( his_data,  max_hold, base_code, start_day = "", end_da
             if one_hold.code not in md_that_day:
                 # 当日该code已经不在指数成份里
                 to_sell.append( one_hold.seq)
-                #print "卖出 %s，因为不在成份里" % one_hold.code
+                #print "%s,卖出 %s，因为不在成份里" % (t_day, one_hold.code)
                 continue
-                
 
-            rank, pos = get_rank(one_hold.code, sorted_y_indices, WHICH_INDI)   #可‘并列’的名次
-            
-            y_indices_of_we_hold = sorted_y_indices[pos][1]  #该持仓代码的昨日指标
-
-            if rank <= sell_threshold and y_indices_of_we_hold[WHICH_INDI] > 0:
+            if one_hold.code in to_buy:
+                # 太糟了
                 to_hold.append( one_hold.seq)
-                #print "持有 %s，排名%d" % (one_hold.code, rank)
+                to_buy.remove( one_hold.code)
+                #print "%s, 继续持有 %s" % (t_day, one_hold.code)
             else:
                 to_sell.append( one_hold.seq)
-                #print "卖出 %s，排名%d" % (one_hold.code, rank)
 
         to_hold_codes = we_hold.get_codes_from_holds(  to_hold)
-
-        # 撸一遍昨日M强，看看有哪些要买进
-        max_buy = max_hold - len(to_hold)
-        rank = 1
-        for code,indi  in sorted_y_indices:
-            if max_buy <=0 :
-                break
-
-            # 比较基准不买
-            if code == FH_BASE_CODE:
-                break
-            
-            # 指标为负不买
-            if indi[WHICH_INDI] <= 0 :
-                break
-            
-            if code in to_hold_codes  or not indi[0]:
-            #                         可买标志  
-                continue
-            
-            if code not in md_that_day:
-                # 当日该code已经不在指数成份里
-                continue
-
-            #print "买入 %s，排名%d" % (code, rank)
-            to_buy.append(code)
-            max_buy = max_buy - 1
-            rank = rank + 1
-
         to_sell_codes = we_hold.get_codes_from_holds( to_sell)
         
         op_num = len(to_buy) + len(to_sell)
@@ -1551,14 +1585,15 @@ def sim_rotate_buy_worst( his_data,  max_hold, base_code, start_day = "", end_da
         blank_num = blank_num + blank_howmany 
 
         # 开始调整we_hold  估算当日的净值
-        
         for one_pos in we_hold.pos_entries:
             if one_pos.seq in to_sell:
                 # 要卖掉
-                
-                # ‘行情’ 是  [收盘价，前日收盘，涨幅， 涨停标志，停牌标志]
-                trade_price = y_md[one_pos.code ][0]  #  FIXME: 如果该code停牌，这里需要寻找到其复牌价
-                
+
+                trade_price =  smart_get_md_close(y_date,one_pos.code,y_md)  #  FIXME: 如果该code停牌，这里需要寻找到其复牌价
+                if math.isnan( trade_price):
+                    print "WARN: %s , %s 的昨收盘是NaN，只能以最后的持仓价格卖出" % (t_day, one_pos.code)
+                    trade_price = one_pos.now_price 
+
                 trade_amount = one_pos.volumn * trade_price
                 trade_loss   = trade_amount *  ( TRADE_COST + TRADE_TAX) 
 
@@ -1579,6 +1614,8 @@ def sim_rotate_buy_worst( his_data,  max_hold, base_code, start_day = "", end_da
         # 买进操作
         if len(to_buy):
             each = we_hold.remaining / we_hold.get_blank_num()
+        if math.isnan(each):
+            raise Exception( "%s, remaining=%f, black_num = %d"   % (t_day, we_hold.remaining,we_hold.get_blank_num() )   )
 
         for one_buy in to_buy:
             pos = we_hold.find_first_blank_pos()
@@ -1613,10 +1650,11 @@ def sim_rotate_buy_worst( his_data,  max_hold, base_code, start_day = "", end_da
         #print r_that_day
         #print we_hold
         #print 
-
         trans_num = trans_num + op_num 
+        
+        real_tday_num  = real_tday_num + 1
 
-    print "平均每天持有仓数 %d, 每天空仓数 %d" % (hold_num / len(his_data) , blank_num/len(his_data))
+    print "平均每天持有仓数 %f, 每天空仓数 %f" % (float(hold_num) / len(his_data) , float(blank_num)/len(his_data))
 
     return (result ,  trans_num , trans_cost )
 
@@ -1684,7 +1722,7 @@ def fh50_until_now(engine, start_year):
             )
 
 
-def sh50_buy_worst(engine, start_day, end_day, howlong):   # N日涨幅最遭
+def sh50_buy_worst(engine, start_day, end_day, max_hold, howlong):   # N日涨幅最遭
 
     #从DB抓日线数据
     conn = engine.connect()
@@ -1705,13 +1743,11 @@ def sh50_buy_worst(engine, start_day, end_day, howlong):   # N日涨幅最遭
 #     T_day3, {证券1:证券1的行情, 证券2:证券2的行情, ... }, {证券1:证券1的行情, 证券2:证券2的行情, ... }
 #     ...
 
-    howlong = 20 
 
     # ‘指标’ 是  [可买标志，N日累计涨幅]
     make_indices_by_delta2( conn,  his_md , howlong)
  
-
-    result, trans_num, trans_cost  = sim_rotate( his_md, 45 , FH_BASE_CODE, start_day, end_day )    
+    result, trans_num, trans_cost  = sim_rotate_buy_worst( his_md, max_hold , howlong, FH_BASE_CODE, start_day, end_day )    
 # Output: 2-D array , 交易数， 交易成本
 #         日期  基准收盘价   策略净值 交易次数  换仓详细  
 #         ...
@@ -1742,8 +1778,8 @@ def sh50_buy_worst(engine, start_day, end_day, howlong):   # N日涨幅最遭
     base_delta   = result[ t_day_num - 1][1] / result[ 0][1]
     policy_delta = result[ t_day_num - 1][2] / result[ 0][2]
 
-    print "MA%d上方骑快马, %s ~ %s, %d个交易日，交易%d笔，交易成本%f，基准表现%f，策略表现%f" % (
-             ma_size
+    print "%d日最糟, %s ~ %s, %d个交易日，交易%d笔，交易成本%f，基准表现%f，策略表现%f" % (
+             howlong
             , result[0][0], result[ t_day_num - 1][0], t_day_num
             , trans_num,  trans_cost
             , base_delta, policy_delta 
@@ -2212,11 +2248,10 @@ def handle_sh50( argv, argv0 ):
 
         # get db engine
         engine = db_operator.get_db_engine()
-        
-        
 
         end_day = ''
-        ma_size = 5
+        max_hold = 5
+        ma_size  = 20
 
         i = len(argv)
         if ( 0 == i  ):
@@ -2233,10 +2268,7 @@ def handle_sh50( argv, argv0 ):
     
         #fh50_until_now(engine, start_year)   # N日涨幅最强
         
-        sh50_buy_worst(engine, start_day, end_day, ma_size)   # N日涨幅最遭
-
-
-
+        sh50_buy_worst(engine, start_day, end_day,max_hold, ma_size)   # N日涨幅最遭
 
     except  Exception as e:
         (t, v, bt) = sys.exc_info()
@@ -2247,7 +2279,5 @@ def handle_sh50( argv, argv0 ):
     finally:
         pass
 
-
     return 0
-
 
